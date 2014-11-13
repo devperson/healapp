fa5098b523a68865fc7e398fbc4919cfd0031646
sadas <asdas@fggf.fd>
2014-11-13 14:36:52 +0700

    back press fixed

--- HealthDemo/HealthDemo.Android/MainActivity.cs
+++ HealthDemo/HealthDemo.Android/MainActivity.cs
@@ -12,6 +12,10 @@ using Xamarin.Forms.Labs.Droid;
 using Xamarin.Forms;
 using Xamarin;
 using Android.Content;
+using XLabs.Ioc;
+using Xamarin.Forms.Labs.Mvvm;
+using Xamarin.Forms.Labs;
+using XLabs.Serialization;
 
 namespace HealthDemo.Droid
 {
@@ -22,9 +26,41 @@ namespace HealthDemo.Droid
         {
             base.OnCreate(bundle);
 
+            if (!Resolver.IsSet)
+            {
+                this.SetIoc();
+            }
+            else
+            {
+                var app = Resolver.Resolve<IXFormsApp>() as IXFormsApp<XFormsApplicationDroid>;
+                app.AppContext = this;
+            }
+
             Forms.Init(this, bundle);
             FormsMaps.Init(this, bundle);
             SetPage(App.GetMainPage());
+        }
+
+        /// <summary>
+        /// Sets the IoC.
+        /// </summary>
+        private void SetIoc()
+        {
+            var resolverContainer = new SimpleContainer();
+
+            var app = new XFormsAppDroid();
+            app.Init(this);
+            resolverContainer.Register<IDevice>(t => AndroidDevice.CurrentDevice)
+                .Register<IDisplay>(t => t.Resolve<IDevice>().Display)
+                //.Register<IJsonSerializer, Services.Serialization.JsonNET.JsonSerializer>()
+                //.Register<IJsonSerializer, XLabs.Serialization.ServiceStack.JsonSerializer>()
+                .Register<IDependencyContainer>(resolverContainer)
+                .Register<IXFormsApp>(app);
+                //.Register<ISimpleCache>(
+                //    t => new SQLiteSimpleCache(new SQLite.Net.Platform.XamarinAndroid.SQLitePlatformAndroid(),
+                //        new SQLite.Net.SQLiteConnectionString(pathToDatabase, true), t.Resolve<IJsonSerializer>()));
+
+            Resolver.SetResolver(resolverContainer.GetResolver());
         }
     }
 }
